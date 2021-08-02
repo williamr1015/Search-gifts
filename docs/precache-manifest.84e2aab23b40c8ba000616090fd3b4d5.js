a6c5bb6ca999eacf929fda9da02943b2592447fa
williamr1015 <william.rodriguez@alteru.co>
2021-08-02 13:31:09 -0500

    se agrega docs

---
+++ docs/precache-manifest.84e2aab23b40c8ba000616090fd3b4d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba254d0ec7cfb27933df4e9037b5b40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59a4d776cdb114aaa672",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59a4d776cdb114aaa672",
+    "url": "/static/js/main.b1e15466.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
